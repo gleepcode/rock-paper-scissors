5b1a5898c21eaa139bd1b5a9d01b4189b909b76d
gleepcode <196574880+gleepcode@users.noreply.github.com>
2026-01-01 15:23:49 -0400

    Create function to get computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello!");
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"]
+    randomChoice = Math.floor(Math.random() * 3);
+    return choices.at(randomChoice);
+}
+
+// Testing
+console.log(getComputerChoice());
