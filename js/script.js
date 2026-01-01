d7708f9a8776199a6f52106788b771f33918be40
gleepcode <196574880+gleepcode@users.noreply.github.com>
2026-01-01 15:31:23 -0400

    Create function to get player choice from input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
-    randomChoice = Math.floor(Math.random() * 3);
+    const randomChoice = Math.floor(Math.random() * 3);
     return choices.at(randomChoice);
 }
 
+function getHumanChoice() {
+    const promptMessage = "Rock, paper or scissors?";
+    const choice = prompt(promptMessage)
+    return choice;
+}
+
+
 // Testing
 console.log(getComputerChoice());
+console.log(getHumanChoice());
