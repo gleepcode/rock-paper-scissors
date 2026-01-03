b4067f2ddf17deb16deaadb5962c384408d53551
gleepcode <196574880+gleepcode@users.noreply.github.com>
2026-01-02 20:59:17 -0400

    Add playGame function

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 For this project I'll be creating rock paper scissors, played in the console. This is for the Odin Project.
 
-## To do
-
-- Use while loop to run rounds until 5 points
-
 ## Log
 
 2026-01-01
 - Working on the playRound function
 2026-01-02
 - playRound complete, can play single rounds
+- playGame complete, can play a best of 5
--- js/script.js
+++ js/script.js
@@ -1,81 +1,93 @@
-let playerScore = 0;
-let computerScore = 0;
+function playGame() {
 
-function playRound() {
+    let playerScore = 0;
+    let computerScore = 0;
 
-    function getComputerChoice() {
-        const choices = ["rock", "paper", "scissors"]
-        const randomChoice = Math.floor(Math.random() * 3);
-        return choices.at(randomChoice);
-    }
+    function playRound() {
 
-    function getPlayerChoice() {
-        const promptMessage = "Rock, paper or scissors?";
-        const choice = prompt(promptMessage).toLowerCase();
-        return choice;
-    }
-
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-
-    function win() {
-        console.log(`${playerChoice} beats ${computerChoice}, you won the round!`);
-        playerScore += 1;
-    }
-    function lose() {
-        console.log(`${computerChoice} beats ${playerChoice}, you lost the round...`);
-        playerScore += 1;
-    }
-    function tie() {
-        console.log(`${playerChoice} ties with ${computerChoice}, it's a draw.`);
-    }
-    
-    if (playerChoice === "rock") {
-        switch (computerChoice) {
-            case "rock":
-                tie();
-                break;
-            case "paper":
-                lose();
-                break;
-            case "scissors":
-                win();
-                break;
+        function getComputerChoice() {
+            const choices = ["rock", "paper", "scissors"]
+            const randomChoice = Math.floor(Math.random() * 3);
+            return choices.at(randomChoice);
         }
-    } else if (playerChoice === "paper") {
-        switch (computerChoice) {
-            case "rock":
-                win();
-                break;
-            case "paper":
-                tie();
-                break;
-            case "scissors":
-                lose();
-                break;
+
+        function getPlayerChoice() {
+            const promptMessage = "Rock, paper or scissors?";
+            const choice = prompt(promptMessage).toLowerCase();
+            return choice;
         }
-    } else if (playerChoice === "scissors") {
-        switch (computerChoice) {
-            case "rock":
-                lose();
-                break;
-            case "paper":
-                win();
-                break;
-            case "scissors":
-                tie();
-                break;
+
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+
+        function win() {
+            console.log(`${playerChoice} beats ${computerChoice}, you won the round!`);
+            playerScore += 1;
         }
+        function lose() {
+            console.log(`${computerChoice} beats ${playerChoice}, you lost the round...`);
+            computerScore += 1;
+        }
+        function tie() {
+            console.log(`${playerChoice} ties with ${computerChoice}, it's a draw.`);
+        }
+        
+        if (playerChoice === "rock") {
+            switch (computerChoice) {
+                case "rock":
+                    tie();
+                    break;
+                case "paper":
+                    lose();
+                    break;
+                case "scissors":
+                    win();
+                    break;
+            }
+        } else if (playerChoice === "paper") {
+            switch (computerChoice) {
+                case "rock":
+                    win();
+                    break;
+                case "paper":
+                    tie();
+                    break;
+                case "scissors":
+                    lose();
+                    break;
+            }
+        } else if (playerChoice === "scissors") {
+            switch (computerChoice) {
+                case "rock":
+                    lose();
+                    break;
+                case "paper":
+                    win();
+                    break;
+                case "scissors":
+                    tie();
+                    break;
+            }
+        }
+
+        function printScores() {
+            console.log(`Current score:`);
+            console.log(`Player: ${playerScore}`);
+            console.log(`Computer: ${computerScore}`);
+        }
+
+        printScores();
     }
 
-    function printScores() {
-        console.log(`Current score:`);
-        console.log(`Player: ${playerScore}`);
-        console.log(`Computer: ${computerScore}`);
+    while (playerScore < 5 && computerScore < 5) {
+        playRound();
     }
 
-    printScores();
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    } else {
+        console.log("You lose!");
+    }
 }
 
-// Gameplay
-playRound();
+playGame();
